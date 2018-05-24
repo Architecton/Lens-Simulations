becf9b99139419a2b374d498bfde638c165f6205
Jernej Vivod <je.vivod@gmail.com>
2018-05-24 15:44:47 +0200

    Fix parameters

--- Octave-Version/screens/get_pixel_coordinates.m
+++ Octave-Version/screens/get_pixel_coordinates.m
@@ -7,15 +7,15 @@
 % scaling_vector_2 ... the 2. first screen spanning vector
 %
 % Author: Jernej Vivod
-function [pixel_coordinates] = get_pixel_coordinates(screen_lu_coordinates, scaling_vector_1, scaling_vector_2, image)
+function [pixel_coordinates] = get_pixel_coordinates(screen_lu_coordinates, scaling_vector_1, scaling_vector_2, image_dimensions)
   
   % Get image dimensions.
-  image_y_dim = size(image)(1);
-  image_x_dim = size(image)(2);
+  image_y_dim = image_dimensions(1);
+  image_x_dim = image_dimensions(2);
   
   % Compute matrices of indices.
   [X, Y] = meshgrid(linspace(0, image_x_dim - 1, image_x_dim), linspace(0, image_y_dim - 1, image_y_dim));
   
   % Get coordinates of each pixel of the screen (the image matrix)
-  pixel_coordinates = arrayfun(@(i, j) get_coordinates(i, j, scaling_vector_1, scaling_vector_2, size(image), screen_lu_coordinates), Y, X, 'UniformOutput', false);
+  pixel_coordinates = arrayfun(@(i, j) get_coordinates(i, j, scaling_vector_1, scaling_vector_2, image_dimensions, screen_lu_coordinates), Y, X, 'UniformOutput', false);
 endfunction
